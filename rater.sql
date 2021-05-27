4c722e96d6b65649aa484f1efb91c6365e09101f
sllittrell <stacey.littrell@gmail.com>
2021-05-27 16:07:21 -0500

    raterprojects_report created

--- rater.sql
+++ rater.sql
@@ -2,3 +2,4 @@ SELECT * FROM raterprojectapi_game
 SELECT * FROM raterprojectapi_review
 SELECT * FROM raterprojectapi_rating
 
+Select 
---
+++ raterproject_reports/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RaterprojectReportsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'raterproject_reports'
